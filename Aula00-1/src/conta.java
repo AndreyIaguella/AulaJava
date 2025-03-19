2c65a593811066fe098b4eb57b45bdb164600015
ANDREY GUSTAVO DE MELLO IAGUELLA <andrey.iaguella@cs.up.edu.br>
2025-03-18 21:47:45 -0300

    Signed-off-by: ANDREY GUSTAVO DE MELLO IAGUELLA <andrey.iaguella@cs.up.edu.br>

---
+++ Aula00-1/src/conta.java
@@ -0,0 +1,21 @@
+public class conta {
+
+    public String titulo;
+
+    public Integer numero;
+
+    public String agencia;
+
+    public double saldo;
+
+    public String abertura;
+
+    public void imprimir(){
+        System.out.println("Titulo: " + titulo);
+        System.out.println("número: " + numero);
+        System.out.println("Agência: " + agencia);
+        System.out.println("Saldo: " + saldo);
+        System.out.println("abertura: " + abertura);
+    }
+
+}
---
+++ Aula00-1/src/setConta.java
@@ -0,0 +1,16 @@
+public class setConta {
+
+    public static void main(String[] args) {
+
+        conta cliente = new conta();
+        cliente.titulo = "AAAA";
+        cliente.abertura = "1";
+        cliente.agencia = "abc";
+        cliente.numero = 123;
+        cliente.saldo = 321;
+
+        cliente.imprimir();
+
+    }
+
+    }
